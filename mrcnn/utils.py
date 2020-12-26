277422f80de3e6a19c19d22058843fc5bae9924e
ketnas <36123831+ketnas@users.noreply.github.com>
2020-12-26 22:48:53 +0700

    Update utils.py

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -24,9 +24,9 @@ import warnings
 from distutils.version import LooseVersion
 
 # URL from which to download the latest COCO trained weights
-COCO_MODEL_URL = "https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5"
+# COCO_MODEL_URL = "https://github.com/matterport/Mask_RCNN/releases/download/v2.0/mask_rcnn_coco.h5"
 
-
+COCO_MODEL_URL = "https://github.com/ketnas/fashion-test/releases/download/weights/mask_rcnn_deepfashion2_0100.h5"
 ############################################################
 #  Bounding Boxes
 ############################################################
